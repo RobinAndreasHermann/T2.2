532fb37d246fc7e03f3ccdcf0c9d8c6d4074bb77
Robin <robin.hermann@rub.de>
2021-10-28 12:45:50 +0200

    Class StudentList for 2.6 & Constructors for 2.7

--- src/exercises/StudentList.java
+++ src/exercises/StudentList.java
@@ -1,4 +1,20 @@
 package exercises;
 
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentList {
+
+    ArrayList<Student> students = new ArrayList<Student>();
+
+    StudentList(){
+
+    }
+
+    StudentList(StudentList studentList){
+        for(Student s : studentList.students){
+            this.students.add(s);
+        }
+    }
 }
