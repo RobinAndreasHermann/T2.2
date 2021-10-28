1673d0d446e647c700e06d8efedc3f94deb45d0d
Robin <robin.hermann@rub.de>
2021-10-28 13:30:53 +0200

    first 3 methods implemented boolean add boolean remove Student remove

--- src/exercises/StudentList.java
+++ src/exercises/StudentList.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class StudentList {
 
-    ArrayList<Student> students = new ArrayList<Student>();
+    ArrayList<Student> students = new ArrayList<>();
 
     StudentList(){
 
@@ -16,5 +16,35 @@ public class StudentList {
         for(Student s : studentList.students){
             this.students.add(s);
         }
+    }
+
+    //Methods for 2.8
+    boolean add(Student student){
+        for (Student s : students) {
+            if (s.getStudentId() == student.getStudentId()){
+                return false;
+            }
+        }
+        students.add(student);
+        return true;
+    }
+
+    boolean remove(Student student){
+        for (Student s : students) {
+            if (s.getStudentId() == student.getStudentId()){
+                students.remove(s);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    Student remove(int pos){
+        if(0 <= pos && pos < students.size()){
+            Student s = students.get(pos);
+            students.remove(s);
+            return s;
+        }
+        return null;
     }
 }
